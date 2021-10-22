cb3497a4640331a4132490c5f96f36710105bb94
mak100un <44783367+mak100un@users.noreply.github.com>
2021-10-22 23:43:46 +0300

    add fonts

---
+++ LeadersOfDigital/LeadersOfDigital.Android/Activities/Autorization/AuthorizationActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LeadersOfDigital.Android.Activities
+{
+    [Activity(Label = "AuthorizationActivity")]
+    public class AuthorizationActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
