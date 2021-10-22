a5e8ebb0b70aa060a292448ffcf2142e001446e0
swamp1820 <swamp1820@yandex.ru>
2021-10-23 00:53:15 +0300

    add hotels API

---
+++ LeadersApi/Models/Responses/Hotels/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeadersApi.Models.Responses.Hotels
+{
+    public class Hotel
+    {
+        public int hotelId { get; set; }
+
+        public int stars { get; set; }
+
+        public double priceAvg { get; set; }
+        public string hotelName { get; set; }
+
+        public double priceFrom { get; set; }
+
+        public Location location { get; set; }
+
+       
+    }
+}
---
+++ LeadersOfDigital.DataModels/Responses/Hotels/HotelsResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeadersOfDigital.DataModels.Responses.Hotels
+{
+    public class HotelsResponse
+    {
+        public int HotelId { get; set; }
+
+        public int Stars { get; set; }
+
+        public double PriceAvg { get; set; }
+        public string HotelName { get; set; }
+
+        public double PriceFrom { get; set; }
+
+        public float Lat { get; set; }
+
+        public float Lon { get; set; }
+    }
+}
