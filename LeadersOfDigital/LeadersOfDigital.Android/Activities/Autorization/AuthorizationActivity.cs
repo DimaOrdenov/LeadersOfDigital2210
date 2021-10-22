a836ce2d135653674f78ad121b523ad60f0edc2d
mak100un <44783367+mak100un@users.noreply.github.com>
2021-10-23 00:45:42 +0300

    complete auth activity

--- LeadersOfDigital/LeadersOfDigital.Android/Activities/Autorization/AuthorizationActivity.cs
+++ LeadersOfDigital/LeadersOfDigital.Android/Activities/Autorization/AuthorizationActivity.cs
@@ -1,6 +1,8 @@
 using Android.App;
 using Android.OS;
-using LeadersOfDigital.ViewModels;
+using Android.Widget;
+using LeadersOfDigital.ViewModels.Authorization;
+using MvvmCross.Platforms.Android.Binding;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
 
@@ -10,12 +12,34 @@ namespace LeadersOfDigital.Android.Activities
     [Activity(Label = "AuthorizationActivity")]
     public class AuthorizationActivity : MvxActivity<AuthorizationViewModel>
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.AuthorizationActivity);
 
+            var set = CreateBindingSet();
 
+            set.Bind(FindViewById<Button>(Resource.Id.authorize_button))
+                .For(x => x.BindClick())
+                .To(vm => vm.AuthorizationCommand);
+
+            set.Bind(FindViewById<TextView>(Resource.Id.skip_auth_textview))
+                .For(x => x.BindClick())
+                .To(vm => vm.SkipAuthCommand);
+
+           foreach (var networkButton in new[]
+           {
+                FindViewById<ImageButton>(Resource.Id.odnoklassniki_button),
+                FindViewById<ImageButton>(Resource.Id.sbarbank_button),
+                FindViewById<ImageButton>(Resource.Id.vk_button),
+                FindViewById<ImageButton>(Resource.Id.telegram_button),
+                FindViewById<ImageButton>(Resource.Id.yandex_button)
+            })
+            {
+                set.Bind(networkButton)
+                   .For(x => x.BindClick())
+                   .To(vm => vm.AuthViaSocialNetworkCommand);
+            }
         }
     }
 }
